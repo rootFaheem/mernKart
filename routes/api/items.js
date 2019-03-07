bbb07d2986cd5bec973ccd9f207e2d011962653f
Faheem <faheemtech1@gmail.com>
2019-03-08 01:38:48 +0530

    create, delete request api working

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,13 +14,22 @@ router.get("/", (req, res) => {
 });
 
 //  @route  POST api/items
-//  @desc    Adds a item
+//  @desc   Creates an Item
 //  @access Public
 router.post("/", (req, res) => {
   const newItem = new Item({
     name: req.body.name
   });
   newItem.save().then(item => res.json(item));
+});
+
+//  @route  DELETE api/items
+//  @desc   Deletes an Item
+//  @access Public
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
