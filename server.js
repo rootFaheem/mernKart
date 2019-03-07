ff2981976e51739edfc30a3d04542edd54a7641a
Faheem <faheemtech1@gmail.com>
2019-03-08 00:14:26 +0530

    server ready | MongoDB connected

--- server.js
+++ server.js
@@ -9,3 +9,13 @@ app.use(bodyParser.json());
 
 // DB config
 const db = require("./config/keys").mongoURI;
+
+// DB config
+mongoose
+  .connect(db)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
+
+// server running
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server is running on port ${port}`));
