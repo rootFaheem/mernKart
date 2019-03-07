69a4bb21a08705bb2a5885e72f0f360cfe8b40da
Faheem <faheemtech1@gmail.com>
2019-03-08 01:28:13 +0530

    item creation and saving to DB is done

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,13 +4,23 @@ const router = express.Router();
 // Item Model
 const Item = require("../../models/Item");
 
-// @route   GET api/items
+//  @route  GET api/items
 //  @desc   Get All Items
 //  @access Public
 router.get("/", (req, res) => {
   Item.find()
     .sort({ date: -1 })
     .then(items => res.json(items));
+});
+
+//  @route  POST api/items
+//  @desc    Adds a item
+//  @access Public
+router.post("/", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+  newItem.save().then(item => res.json(item));
 });
 
 module.exports = router;
